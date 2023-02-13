626af07803d7002ea109bde171457bad882e7492
Nam Tran <84835762+nnamtran@users.noreply.github.com>
2023-02-13 16:04:41 +1100

    use AWS S3

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-require('dotenv').config()
+const dotenv = require('dotenv')
 const path = require('path')
 PORT = process.env.PORT || 3004;
 
@@ -6,17 +6,37 @@ const express = require("express")
 const multer = require("multer")
 const cors = require("cors");
 
+const fs = require('fs')
+
+const { S3Client, PutObjectCommand, GetObjectCommand } = require("@aws-sdk/client-s3");
+const { getSignedUrl } = require('@aws-sdk/s3-request-presigner');
 const {MongoClient} = require('mongodb')
 const uri = "mongodb+srv://vitachimart:vitachimartecommerce@cluster0.eep3pnp.mongodb.net/?retryWrites=true&w=majority";
 
+dotenv.config()
 
+
+
+// ----- Start the express app
 const app = express()
 app.use(express.json())
 app.use(cors());
 
-const fs = require("fs");
+// ----- Configure the S3 Bucket
+const bucketName = process.env.BUCKET_NAME
+const bucketRegion = process.env.BUCKET_REGION
+const accessKey = process.env.ACCESS_KEY
+const secretAccessKey = process.env.SECRET_ACCESS_KEY
 
+const s3 = new S3Client({
+    credentials: {
+        accessKeyId: accessKey,
+        secretAccessKey: secretAccessKey,
+    },
+    region: bucketRegion
+})
 
+// ----- Using Multer to upload image
 var storage = multer.memoryStorage();
         
 var upload = multer({ 
@@ -26,7 +46,150 @@ var upload = multer({
 app.get("/",function(req,res){
     res.render("Signup");
 })
+
+// ------ Get all the products
+app.get('/allproducts', async (req, res, next) => {
+    const client = new MongoClient(uri)
+    const productCategory = [];
+
+    // let data = fs.readFileSync('./data/all.txt');
+    // let response = JSON.parse(data);
+    //console.log(response);
     
+    try {
+        await client.connect()
+        const database = client.db('vitachimart-products');
+        const products = database.collection('products')
+
+        
+        const baby = await products.find({"category": "baby"}).project({
+            image: 0
+        }).toArray();
+        baby.forEach((item) => {
+            item.imageUrl = `https://vitachi-products-image.s3.ap-southeast-2.amazonaws.com/${item._id.toString()}`
+        })
+        const pregnancy = await products.find({"category": "pregnancy"}).project({
+            image: 0
+        }).toArray();
+        pregnancy.forEach(async (item) => {
+            item.imageUrl = `https://vitachi-products-image.s3.ap-southeast-2.amazonaws.com/${item._id.toString()}`
+        })
+        const vitamins = await products.find({"category": "vitamins"}).project({
+            image: 0
+        }).toArray();
+        vitamins.forEach(async (item) => {
+            item.imageUrl = `https://vitachi-products-image.s3.ap-southeast-2.amazonaws.com/${item._id.toString()}`
+        })
+        const women = await products.find({"category": "women"}).project({
+            image: 0
+        }).toArray();
+        women.forEach(async (item) => {
+            item.imageUrl = `https://vitachi-products-image.s3.ap-southeast-2.amazonaws.com/${item._id.toString()}`
+        })
+        const elderly = await products.find({"category": "elderly"}).project({
+            image: 0
+        }).toArray();
+        elderly.forEach(async (item) => {
+            item.imageUrl = `https://vitachi-products-image.s3.ap-southeast-2.amazonaws.com/${item._id.toString()}`
+        })   
+        const men = await products.find({"category": "men"}).project({
+            image: 0
+        }).toArray();
+        men.forEach(async (item) => {
+            item.imageUrl = `https://vitachi-products-image.s3.ap-southeast-2.amazonaws.com/${item._id.toString()}`
+        })
+        
+
+        productCategory.push({
+            vitamins,
+            baby,
+            elderly,
+            men,
+            pregnancy,
+            women
+        })
+
+        
+    } finally {
+        await client.close()
+    }
+    res.send(productCategory);
+})
+
+// ----- Get array of 6 items for each category - landingpage
+app.get('/landingpage', async (req, res, next) => {
+    const client = new MongoClient(uri)
+    const productCategory = [];
+    
+    try {
+        await client.connect()
+        const database = client.db('vitachimart-products');
+        const products = database.collection('products')
+
+        // Baby
+        const baby = await products.find({"category": "baby"}).project({
+            image: 0
+        }).limit(6).toArray();
+        baby.forEach((item) => {
+            item.imageUrl = `https://vitachi-products-image.s3.ap-southeast-2.amazonaws.com/${item._id.toString()}`
+        })
+
+        //Pregnancy
+        const pregnancy = await products.find({"category": "pregnancy"}).project({
+            image: 0
+        }).limit(6).toArray();
+        pregnancy.forEach(async (item) => {
+            item.imageUrl = `https://vitachi-products-image.s3.ap-southeast-2.amazonaws.com/${item._id.toString()}`
+        })
+
+        // Vitamins
+        const vitamins = await products.find({"category": "vitamins"}).project({
+            image: 0
+        }).limit(6).toArray();
+        vitamins.forEach(async (item) => {
+            item.imageUrl = `https://vitachi-products-image.s3.ap-southeast-2.amazonaws.com/${item._id.toString()}`
+        })
+
+        // Women
+        const women = await products.find({"category": "women"}).project({
+            image: 0
+        }).limit(6).toArray();
+        women.forEach(async (item) => {
+            item.imageUrl = `https://vitachi-products-image.s3.ap-southeast-2.amazonaws.com/${item._id.toString()}`
+        })
+
+        // Men
+        const men = await products.find({"category": "men"}).project({
+            image: 0
+        }).limit(6).toArray();
+        men.forEach(async (item) => {
+            item.imageUrl = `https://vitachi-products-image.s3.ap-southeast-2.amazonaws.com/${item._id.toString()}`
+        })
+
+        // Elderly
+        const elderly = await products.find({"category": "elderly"}).project({
+            image: 0
+        }).limit(6).toArray();
+        elderly.forEach(async (item) => {
+            item.imageUrl = `https://vitachi-products-image.s3.ap-southeast-2.amazonaws.com/${item._id.toString()}`
+        })   
+        
+        // Push every into an object
+        productCategory.push({
+            vitamins,
+            baby,
+            elderly,
+            men,
+            pregnancy,
+            women
+        })  
+    } finally {
+        await client.close()
+    }
+    res.send(productCategory)
+})
+
+// ----- Add product for vitachi-additem
 app.post("/addproduct", upload.single("image"), async (req, res, next) => {
 
     const client = new MongoClient(uri)
@@ -57,16 +220,14 @@ app.post("/addproduct", upload.single("image"), async (req, res, next) => {
         } else {
             const insertedProduct = await register.insertOne(data);
             res.status(200).send("success");
-        }
-
-        
-        
+        } 
 
     } catch(error) {
         console.log(error)
     }
 })
 
+// ----- Find the product in Vitachi-additem
 app.get('/products', async (req, res, next) => {
     const client = new MongoClient(uri)
     const product_name = req.query.getProduct.name;
@@ -85,92 +246,6 @@ app.get('/products', async (req, res, next) => {
     }
 })
 
-app.get('/landingpage', async (req, res, next) => {
-    const client = new MongoClient(uri)
-    const productCategory = [];
-
-    let data = fs.readFileSync('./data/test.txt');
-    let response = JSON.parse(data);
-    //console.log(response);
-    res.send(response);
-    // try {
-    //     await client.connect()
-    //     const database = client.db('vitachimart-products');
-    //     const products = database.collection('products')
-
-    //     //const product = await products.find().limit(12).toArray();
-
-    //     const vitamins = await products.find({"category": "vitamins"}).limit(6).toArray();
-    //     const baby = await products.find({"category": "baby"}).limit(6).toArray();
-    //     const elderly = await products.find({"category": "elderly"}).limit(6).toArray();
-    //     const men = await products.find({"category": "men"}).limit(6).toArray();
-    //     const pregnancy = await products.find({"category": "pregnancy"}).limit(6).toArray();
-    //     const women = await products.find({"category": "women"}).limit(6).toArray();
-
-    //     productCategory.push({
-    //         vitamins,
-    //         baby,
-    //         elderly,
-    //         men,
-    //         pregnancy,
-    //         women
-    //     })
-
-    //     const jsonData = JSON.stringify(productCategory[0])
-    //     fs.writeFile("./data/test.txt", jsonData, function(err) {
-    //         if (err) {
-    //             console.log(err);
-    //         }
-    //     });
-    //     console.log(productCategory)
-    //     res.send(productCategory)
-    // } finally {
-    //     await client.close()
-    // }
-})
-
-app.get('/allproducts', async (req, res, next) => {
-    const client = new MongoClient(uri)
-
-    const productCategory = [];
-    let data = fs.readFileSync('./data/all.txt');
-    let response = JSON.parse(data);
-    //console.log(response);
-    res.send(response);
-    // try {
-    //     await client.connect()
-    //     const database = client.db('vitachimart-products');
-    //     const products = database.collection('products')
-
-    //     const vitamins = await products.find({"category": "vitamins"}).toArray();
-    //     const baby = await products.find({"category": "baby"}).toArray();
-    //     const elderly = await products.find({"category": "elderly"}).toArray();
-    //     const men = await products.find({"category": "men"}).toArray();
-    //     const pregnancy = await products.find({"category": "pregnancy"}).toArray();
-    //     const women = await products.find({"category": "women"}).toArray();
-
-    //     productCategory.push({
-    //         vitamins,
-    //         baby,
-    //         elderly,
-    //         men,
-    //         pregnancy,
-    //         women
-    //     })
-
-    //     const jsonData = JSON.stringify(productCategory[0])
-    //     fs.writeFile("./data/all.txt", jsonData, function(err) {
-    //         if (err) {
-    //             console.log(err);
-    //         }
-    //     });
-    //     //const product = await products.distinct("category");
-    //     //console.log(product)
-    //     res.send(productCategory)
-    // } finally {
-    //     await client.close()
-    // }
-})
 
 if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
